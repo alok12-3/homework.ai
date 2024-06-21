df43e5349f60d2008a039c02042e208f2e9fbf3d
rishaviitd <ms1210873@iitd.ac.in>
2024-06-22 02:28:11 +0530

    making structure

--- src/pages/DashboardPageTeacher.jsx
+++ src/pages/DashboardPageTeacher.jsx
@@ -13,7 +13,7 @@ const DashboardPageTeacher = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <h1 className="text-4xl font-bold mb-4">Dashboard Page</h1>
-      <p className="text-lg mb-4">Here stats and the most important information will be stored</p>
+      <p className="text-lg mb-4">Here stats will be class wise</p>
       <button
         onClick={goToHomeworks}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
